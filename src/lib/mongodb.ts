d939d056c04e4fa350e6bc9205ddefeb57bd9d3a
pathan taufikkhan <pathan.pathant018.com>
2025-04-11 16:58:42 +0530

    first commit

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,14 +1,28 @@
 // lib/mongodb.ts
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI as string; // in .env.local
-const options = {};
+const uri = process.env.MONGODB_URI as string;
 
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   throw new Error('Add your Mongo URI to .env.local');
 }
 
-const client = new MongoClient(uri, options);
-const clientPromise = client.connect();
+const options = {};
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+// In development, use a global variable so that we don’t create a new client on every hot reload
+if (process.env.NODE_ENV === 'development') {
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  // In production, don't use a global variable
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
 
 export default clientPromise;
