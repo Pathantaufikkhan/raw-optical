03a4d103c9f1a18482789e9a710ac4536867573d
pathan taufikkhan <pathan.pathant018.com>
2025-04-11 17:15:24 +0530

    first commit

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,17 +1,14 @@
 // lib/mongodb.ts
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI as string; // in .env.local
+const uri = process.env.MONGODB_URI as string; // from .env.local
 const options = {};
-
-let client;
-let clientPromise: Promise<MongoClient>;
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Add your Mongo URI to .env.local');
 }
 
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+const client = new MongoClient(uri, options);
+const clientPromise = client.connect();
 
 export default clientPromise;
